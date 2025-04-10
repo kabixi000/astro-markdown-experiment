cde3e65402b73c09e2a511b8a7ba3f29438070f4
kabixi000 <kabixi000@gmail.com>
2025-04-10 14:30:00 +0900

    Add: GitHub Pages のサイトURLを更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   // github pages 用設定
-  site: 'https://kabixi000.github.io',
-  base: '/astro-markdown-experiment'
+  site: 'https://astro.experiment.kabixi000.com',
 })
