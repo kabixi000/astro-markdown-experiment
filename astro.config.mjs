4d3f62a46c1ed69f0a6a46460689126aa6a4d940
kabixi000 <kabixi000@gmail.com>
2025-04-10 13:28:57 +0900

    Add: about.astro ページを追加し、概要を表示するコンテンツを作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  // github pages 用設定
   site: 'https://kabixi000.github.io',
   base: '/astro-markdown-experiment'
 })
