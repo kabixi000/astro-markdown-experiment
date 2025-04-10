1d66123bdd0d7d1663e733f9735cbe46e15a0ca0
kabixi000 <kabixi000@gmail.com>
2025-04-10 11:27:47 +0900

    Add: 初期設定

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://kabixi000.github.io',
+  base: '/astro-markdown-experiment'
+})
